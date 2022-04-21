7b2b37f0de7c97dad101993021ee7fb5b012b358
Ben <bj8th@virginia.edu>
2022-04-21 07:56:12 -0400

    add generate

--- 07_novel_isoform_analysis/run_novel.sh
+++ 07_novel_isoform_analysis/run_novel.sh
@@ -1,5 +1,5 @@
 echo "Generating novel peptide table"
-# python generate_novel_peptide_dataset.py
+python generate_novel_peptide_dataset.py
 
 echo "Generating novel peptide excel data with spectra plots"
 python novel_visualization.py
